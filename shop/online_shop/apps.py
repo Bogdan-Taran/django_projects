0fbfa101c975b03c329241fed08302195c366c69
Bogdan-Taran <taranbogdan9.2@mail.ru>
2025-12-23 17:04:44 +0700

    initialized the application

---
+++ shop/online_shop/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class OnlineShopConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    verbose_name = 'Интернет-мага камапуля ежи'
+    name = 'online_shop'
